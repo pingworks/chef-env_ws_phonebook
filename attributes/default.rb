483b39e64c01ef9b476e2ecab6a7bf7662d905ff
Christoph Lukas <lukas@pingworks.de>
2015-10-29 14:05:51 +0100

    add gemrc files to use local gem mirror

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['pw_base']['ssh-user'] = 'deploy'
 default['pw_base']['ssh-group'] = 'deploy'
 default['pw_base']['ssh-userhome'] = '/home/deploy'
 default['pw_base']['ssh-user-sudo'] = 'deploy	ALL = NOPASSWD: /usr/bin/dpkg -i phonebook*'
+default['pw_base']['gemrc-files'] = ['/root/.gemrc', '/opt/chefdk/embedded/etc/gemrc', '/opt/chef/embedded/etc/gemrc']
